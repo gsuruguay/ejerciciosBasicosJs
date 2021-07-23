f7bebc6c77c6d2e058ec77440312ddb5e23c9357
gsuruguay <guillermo.suruguay@gmail.com>
2021-07-23 00:59:02 -0300

    Se termina ejercicio de arrays

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 const original = ['Rojo', 'Verde', 'Azul', 'Amarillo'];
-// -------
+
 let indiceAmarillo = null;
 let cantidadColoresConA = 0;
 let arregloSinAzul = [];
@@ -7,21 +7,40 @@ let ordenadoPorCantidadLetras = [];
 let arregloConNuevoColorInicio = [];
 let arregloConNuevoColorFin = [];
 
-/*
-    A partir del arreglo original, obtener los siguientes resultados:
-    - indiceAmarillo: Indice de la ubicacion en el arreglo de 'Amarillo'
-    - cantidadColoresConA: Cuantos colores hay en el arreglo con la letra 'a' (Mayuscula o minuscula)
-    - arregloSinAzul: El arreglo original sin el valor 'Azul'
-    - ordenadoPorCantidadLetras: El arreglo orginal ordenado de forma ascendente por la cantidad de letras en su nombre.
-    - arregloConNuevoColorInicio: El arreglo original con un nuevo color al inicio del mismo.
-    - arregloConNuevoColorFin: El arreglo original con un nuevo color al final del mismo.
 
-    Reemplazar este comentario con su codigo.
- */
+//A partir del arreglo original, obtener los siguientes resultados:
 
-console.log(indiceAmarillo);
-console.log(cantidadColoresConA);
-console.log(arregloSinAzul);
-console.log(ordenadoPorCantidadLetras);
-console.log(arregloConNuevoColorInicio);
-console.log(arregloConNuevoColorFin);
+//- indiceAmarillo: Indice de la ubicacion en el arreglo de 'Amarillo'
+original.forEach(element => {
+    if (element == "Amarillo") {
+        indiceAmarillo = original.indexOf("Amarillo");
+    }
+});
+
+//- cantidadColoresConA: Cuantos colores hay en el arreglo con la letra 'a' (Mayuscula o minuscula)
+original.filter(element => (element.toLowerCase().includes("a")) ? cantidadColoresConA++ : 0)
+
+//- arregloSinAzul: El arreglo original sin el valor 'Azul'
+arregloSinAzul = original.filter((element) => element !== 'Azul');
+
+//- ordenadoPorCantidadLetras: El arreglo orginal ordenado de forma ascendente por la cantidad de letras en su nombre.
+ordenadoPorCantidadLetras = original.map(x => x).sort((a,b) => a.length - b.length);
+
+//- arregloConNuevoColorInicio: El arreglo original con un nuevo color al inicio del mismo.
+arregloConNuevoColorInicio = ['Blanco'].concat(original);
+
+//- arregloConNuevoColorFin: El arreglo original con un nuevo color al final del mismo.
+arregloConNuevoColorFin = original.concat(['Negro']);
+
+
+console.log("Indice de la ubicacion en el arreglo de 'Amarillo':", indiceAmarillo);
+
+console.log("Cuantos colores hay en el arreglo con la letra 'a':", cantidadColoresConA);
+
+console.log("El arreglo original sin el valor 'Azul':", arregloSinAzul);
+
+console.log("El arreglo orginal ordenado de forma ascendente por la cantidad de letras en su nombre:", ordenadoPorCantidadLetras);
+
+console.log("El arreglo original con un nuevo color al inicio del mismo:", arregloConNuevoColorInicio);
+
+console.log("El arreglo original con un nuevo color al final del mismo:", arregloConNuevoColorFin);
