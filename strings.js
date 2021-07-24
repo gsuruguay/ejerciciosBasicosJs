dcdc5d9299cb3fb476234fffcaa5b8cd5c2be096
gsuruguay <guillermo.suruguay@gmail.com>
2021-07-23 22:22:58 -0300

    Se termina ejercicio strings

--- strings.js
+++ strings.js
@@ -10,17 +10,31 @@ let fraseOtroNombre = '';
 let posicionNombre = null;
 let arregloParseado = [];
 
-/*
-    A partir de los arreglo originales, obtener los siguientes resultados:
-    - tamanioA: Tamaño del string A
-    - cantidadLetraR: Cuantas letras R (Mayuscula o minuscula) hay en el string A
-    - nombreCompleto: Formar el nombre completo a partir de los strings B y C
-    - fraseOtroNombre: Cambiar el nombre 'Damian' del string D con el nombre del string B
-    - posicionNombre: Posicion de 'Damian' en el string D
-    - arregloParseado: Arreglo formado a partir del string A
 
-    Reemplazar este comentario con su codigo.
- */
+// A partir de los arreglo originales, obtener los siguientes resultados:
+
+// - tamanioA: Tamaño del string A
+tamanioA = stringA.length;
+
+// - cantidadLetraR: Cuantas letras R (Mayuscula o minuscula) hay en el string A
+for (let i = 0; i < stringB.length; i++) {
+    const letra = stringB[i];
+    if (letra.toLowerCase() == "r") {
+        cantidadLetraR++;
+    }
+}
+
+// - nombreCompleto: Formar el nombre completo a partir de los strings B y C
+nombreCompleto = stringB + " " + stringC;
+
+// - fraseOtroNombre: Cambiar el nombre 'Damian' del string D con el nombre del string B
+fraseOtroNombre = stringD.replace("Damian", stringB);
+
+// - posicionNombre: Posicion de 'Damian' en el string D
+posicionNombre = stringD.indexOf("Damian");
+
+// - arregloParseado: Arreglo formado a partir del string A
+arregloParseado = stringA.split(",");
 
 console.log(tamanioA);
 console.log(cantidadLetraR);
