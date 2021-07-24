9b328c9053c9e62450c20cdefde2fc9604c6fda2
gsuruguay <guillermo.suruguay@gmail.com>
2021-07-23 21:22:07 -0300

    Se termina ejercicio objects

--- objects.js
+++ objects.js
@@ -16,21 +16,29 @@ let existeClaveMadreB = null;
 let nombreDelMasJoven = null;
 let clavesDelObjeto = [];
 
-/*
-    A partir de los objetos originales, obtener los siguientes resultados:
-    - edadMadre: Edad de la madre
-    - objetoParseado: Objeto "originalB" parseado a objeto
-    - existeClaveMadreA: Si existe la clave madre en el objeto 'originalA'
-    - existeClaveMadreB: Si existe la clave madre en el objeto 'originalB'
-    - nombreDelMasJoven: Nombre de la persona mas joven
-    - clavesDelObjeto: Que claves contiene el objeto 'originalA'
 
-    Reemplazar este comentario con su codigo.
- */
+//A partir de los objetos originales, obtener los siguientes resultados:
+// - edadMadre: Edad de la madre
+edadMadre = originalA.madre.edad;
 
-console.log(edadMadre);
-console.log(objetoParseado);
-console.log(existeClaveMadreA);
-console.log(existeClaveMadreB);
-console.log(nombreDelMasJoven);
-console.log(clavesDelObjeto);
+// - objetoParseado: Objeto "originalB" parseado a objeto
+objetoParseado = JSON.parse(originalB)
+
+// - existeClaveMadreA: Si existe la clave madre en el objeto 'originalA'
+existeClaveMadreA = (originalA.madre) ? true : false;
+
+// - existeClaveMadreB: Si existe la clave madre en el objeto 'originalB'
+existeClaveMadreB = (originalB.madre) ? true : false;
+
+// - nombreDelMasJoven: Nombre de la persona mas joven
+nombreDelMasJoven = (originalA.edad < objetoParseado.edad) ? nombreDelMasJoven = originalA.nombre : nombreDelMasJoven = objetoParseado.nombre;
+
+// - clavesDelObjeto: Que claves contiene el objeto 'originalA'
+clavesDelObjeto = Object.keys(originalA);
+
+console.log("Edad de la Madre:", edadMadre);
+console.log("Objeto Parseado:", objetoParseado);
+console.log("¿Existe Clave MadreA?:", existeClaveMadreA);
+console.log("¿Existe Clave MadreB?:", existeClaveMadreB);
+console.log("Nombre del mas joven:", nombreDelMasJoven);
+console.log("Claves del objeto originalA:", clavesDelObjeto);
